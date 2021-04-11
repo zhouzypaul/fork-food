65b0d4cdf076b6d285e800cbd81cdf76017d5bf5
seanzhan0319 <seanzhan0319@hotmail.com>
2021-04-11 17:14:17 -0400

    getUserPwd endpoint, changed ForkUtils to static methods in Hub

--- src/main/java/edu/brown/cs/fork/Hub.java
+++ src/main/java/edu/brown/cs/fork/Hub.java
@@ -4,6 +4,10 @@ import edu.brown.cs.fork.actions.ActionLoadDB;
 import edu.brown.cs.fork.database.queries.QueryRestaurants;
 import edu.brown.cs.fork.database.queries.QueryUsers;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * A singleton class containing REPL and databases.
  */
@@ -11,8 +15,13 @@ public class Hub {
   private final Repl repl = new Repl();
   private static final QueryUsers USER_DB = new QueryUsers();
   private static final QueryRestaurants REST_DB = new QueryRestaurants();
-
   private final ActionLoadDB loadDB = new ActionLoadDB();
+
+  // Food categories
+  private static final List<String> CATEGORIES = Arrays.asList(
+      "Burgers", "Chinese", "Pizza", "Italian", "Sushi Bars", "Indian", "Vietnamese", "Steakhouses",
+      "Breakfast & Brunch", "Desserts", "Coffee & Tea", "Greek", "Middle Eastern", "Vegan",
+      "Mexican", "Thai", "American", "Salad", "Barbeque", "Seafood");
 
   /**
    * Constructor.
@@ -33,6 +42,30 @@ public class Hub {
    */
   public static QueryRestaurants getRestDB() {
     return REST_DB;
+  }
+
+  /**
+   * Return a list of categories which is the union of allRestCategories and CATEGORIES.
+   * @param allRestCategories a restaurant's list of food categories
+   * @return union of allRestCategories and CATEGORIES
+   */
+  public static List<String> getOverlap(List<String> allRestCategories) {
+    List<String> overlap = new ArrayList<>();
+    for (String str : allRestCategories) {
+      if (CATEGORIES.contains(str)) {
+        overlap.add(str);
+      }
+    }
+    return overlap;
+  }
+
+  /**
+   * Checks if anyCategory is in our list of categories of interest.
+   * @param anyCategory a category
+   * @return whether anyCategory is in CATEGORIES
+   */
+  public static boolean isInCategories(String anyCategory) {
+    return CATEGORIES.contains(anyCategory);
   }
 
   /**
