269ac390537644477e9c2e568d8acca05d7c0854
agu101 <alan_gu@brown.edu>
2021-04-11 22:35:25 -0400

    Registration and login done

--- src/main/java/edu/brown/cs/fork/handlers/login/LoginHandler.java
+++ src/main/java/edu/brown/cs/fork/handlers/login/LoginHandler.java
@@ -2,6 +2,7 @@ package edu.brown.cs.fork.handlers.login;
 
 import com.google.common.collect.ImmutableMap;
 import com.google.gson.Gson;
+import edu.brown.cs.fork.Hub;
 import edu.brown.cs.fork.users.Person;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -14,6 +15,7 @@ import javax.crypto.spec.PBEKeySpec;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.KeySpec;
+import java.sql.SQLException;
 import java.util.Arrays;
 import java.util.Map;
 
@@ -30,32 +32,34 @@ public class LoginHandler implements Route {
       String username = json.getString("username");
       String password = json.getString("password");
 
-      // TODO: this line subject to change based on Sean
+      // validate password
       boolean authenticated = authenticate(username, password);
 
       Map<String, Object> variables = ImmutableMap.of("success", authenticated);
 
       return GSON.toJson(variables);
 
-    } catch (JSONException | NoSuchAlgorithmException | InvalidKeySpecException e) {
+    } catch (JSONException | NoSuchAlgorithmException | InvalidKeySpecException | SQLException e) {
       System.out.println("ERROR: " + e); // replace with a good exception
     }
-
     return null;
   }
 
-  private boolean authenticate(String username, String password) throws NoSuchAlgorithmException, InvalidKeySpecException {
-//    Person user = getUserByName(username);
-//
-//    byte[] salt = new byte[16];
-//    Arrays.fill(salt, (byte) 0);
-//
-//    KeySpec spec = new PBEKeySpec(password.toCharArray(), salt, 65536, 128);
-//    SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
-//
-//    byte[] hash = factory.generateSecret(spec).getEncoded();
-//
-//    return user.getHash().equals(hash);
-    return true;
+  private boolean authenticate(String username, String password) throws NoSuchAlgorithmException,
+      InvalidKeySpecException, SQLException {
+    // get stored password
+    String storedHash =  Hub.getUserDB().getPwd(username);
+
+    // hash input password
+    byte[] salt = new byte[16];
+    Arrays.fill(salt, (byte) 0);
+
+    KeySpec spec = new PBEKeySpec(password.toCharArray(), salt, 65536, 128);
+    SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+
+    byte[] hash = factory.generateSecret(spec).getEncoded();
+
+    // compare stored and input passwords
+    return storedHash.equals(Arrays.toString(hash));
   }
 }
