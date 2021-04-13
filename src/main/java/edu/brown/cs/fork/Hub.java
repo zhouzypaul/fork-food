801f0dc1a41646311607eed5ff4e976affed895d
Paul Zhou <paul_zhou@brown.edu>
2021-04-12 21:37:57 -0400

    [BUGFIX] resolve Restaurant constructor exception: enforce price range to be 1, 2, 3

--- src/main/java/edu/brown/cs/fork/Hub.java
+++ src/main/java/edu/brown/cs/fork/Hub.java
@@ -18,8 +18,6 @@ import java.util.List;
 import java.util.Set;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 /**
  * A singleton class containing REPL and databases.
