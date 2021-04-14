a18f9b9133b3e3e17935fa93bcf27218f7e4e995
Paul Zhou <paul_zhou@brown.edu>
2021-04-14 11:01:46 -0400

    ranking algorithm

--- src/main/java/edu/brown/cs/fork/Hub.java
+++ src/main/java/edu/brown/cs/fork/Hub.java
@@ -13,12 +13,13 @@ import edu.brown.cs.fork.users.Group;
 import edu.brown.cs.fork.users.Person;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
-
-import java.util.ArrayList;
 
 /**
  * A singleton class containing REPL and databases.
@@ -32,6 +33,7 @@ public class Hub {
   private static final int NUM_RECOMMEND = 10;
   private static final int POSITIVE_CLASS = 1;
   private static final double DEFAULT_RADIUS = 10;
+  private static final double GOTTEN_WAY_PRESERVE_RATIO = 0.7;
 
   // Food categories
   private static final List<String> CATEGORIES = Arrays.asList(
@@ -140,6 +142,63 @@ public class Hub {
     NaiveBayesClassifier<Restaurant, LabeledRestaurant> recAlgo =
             new NaiveBayesClassifier<>(groupPreference, restaurantsWithinRadius);
     return recAlgo.recommend(NUM_RECOMMEND, POSITIVE_CLASS);
+  }
+
+  /**
+   * given the swiping results of users from the frontend, rank the restaurants by how the user
+   * voted on them. The votes of the user are weighted, where each voter has a vote between 0 to 1
+   * on each restaurant (0 means the user's vote doesn't matter at all, and 1 means the user's vote
+   * matters very much). The weight of the vote is determined by the gottenWay field of the user.
+   * This method also updates the gottenWay field of the user in the database after the ranking
+   * is finished.
+   *
+   * @param userIds - a set of userIds
+   * @param votes - a maps from (restaurantId --> (userId --> whether like)), representing the
+   *              swipe results each user had on each restaurant. An integer 1 corresponds to
+   *              "like", and 0 corresponds to "not like"
+   *
+   * @return the top ranking restaurant id
+   */
+  public static String rankRestaurants(Set<String> userIds,
+                                           Map<String, Map<String, Integer>> votes)
+          throws NoUserException, SQLException {
+    // handle invalid inputs
+    if (userIds.size() <= 0 || votes.size() <= 0) {
+      throw new NoUserException("no users/votes passed in to the ranking algorithm");
+    }
+    // get the user's gottenWay from DB
+    Map<String, Float> userGottenWays = new HashMap<>();
+    for (String id : userIds) {
+      userGottenWays.put(id, USER_DB.getUserGottenWay(id));
+    }
+    // rank the restaurants
+    Map<String, Float> votesCount = new HashMap<>();
+    for (String restaurantId : votes.keySet()) {
+      float currentVote = 0;
+      Map<String, Integer> swipeResults = votes.get(restaurantId);
+      for (String userId : swipeResults.keySet()) {
+        currentVote = currentVote + swipeResults.get(userId) * userGottenWays.get(userId);
+      }
+      votesCount.put(restaurantId, currentVote);
+    }
+    // get the highest value in votesCount
+    Map.Entry<String, Float> maxEntry = null;
+    for (Map.Entry<String, Float> entry : votesCount.entrySet()) {
+      if (maxEntry == null || entry.getValue().compareTo(maxEntry.getValue()) > 0) {
+        maxEntry = entry;
+      }
+    }
+    assert maxEntry != null;
+    String highestRankingRestaurant = maxEntry.getKey();
+    // update user's gottenWay in DB
+    for (String userId : userIds) {
+      float newGottenWay = (float) (userGottenWays.get(userId) * GOTTEN_WAY_PRESERVE_RATIO
+              + (1 - votes.get(highestRankingRestaurant).get(userId))
+              * (1 - GOTTEN_WAY_PRESERVE_RATIO));
+      USER_DB.updateUserGottenWay(userId, newGottenWay);
+    }
+    // return top restaurant
+    return highestRankingRestaurant;
   }
 
   /**
