26b0ccc83761fcc17e5f927a841f22109122f8a4
Paul Zhou <paul_zhou@brown.edu>
2021-04-20 10:17:07 -0400

    update message on about page

--- fork-react/src/components/About.js
+++ fork-react/src/components/About.js
@@ -10,9 +10,10 @@ function About() {
       <div className="text">
         <h1 className="header1">what is fork?</h1>
         <p>
-          Fork helps you make decisions in those fork-in-the-road moments. Simply go with a group or go solo. Fork will recommend nearby
-          restaurants based on the group's preferences. All you have to do is swipe and fork will pick the group's favorite restaurant. No more
-          indecisiveness just more eating and enjoying.
+          Fork helps you make decisions in those fork-in-the-road moments. Don't know where to eat? Fork does. Simply go with a group or go solo. Fork will recommend nearby
+          restaurants based on the group's preferences and within your budget. Our AI-backed algo is adaptive, so watch as it learns more
+          about your food preferences and recommend better over time. All you have to do is swipe and fork will pick the group's favorite restaurant.
+          No more indecisiveness just more eating and enjoying.
         </p>
         <h1>the team</h1>
         <ul>
